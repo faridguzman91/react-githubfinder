9b84df5cc1502807e415b85555260bd5a7645cd3
Jimmy Guzman <93182272+faridwoonmodule@users.noreply.github.com>
2022-02-08 23:38:37 +0100

    content, home, about, notfounf 404

---
+++ src/pages/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        App made in react to search github profiles and see profile details
+      </p>
+      <strong>farid guzman</strong>
+      <p className="text-lg text-gray-400">
+        version <strong>1.0.0</strong>
+      </p>
+    </div>
+  );
+}
+
+export default About;
